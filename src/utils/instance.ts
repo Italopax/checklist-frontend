ae543ed29b8adfbdc16f72b031a2d08fc1bfcad6
Italo Paixão Gomes <italopaxg@gmail.com>
2025-04-14 23:21:43 -0300

    feat: add axios

---
+++ src/utils/instance.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export function getServerInstance () {
+  return axios.create({
+    baseURL: 'http://localhost:3030',
+    timeout: 10000,
+    withCredentials: true,
+  });
+}
+
+export function getClientInstance () {
+  return axios.create({
+    baseURL: 'http://localhost:3000',
+    timeout: 10000,
+    withCredentials: true,
+  });
+}
