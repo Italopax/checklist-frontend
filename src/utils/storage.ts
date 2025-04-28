a8d209a666ae79a41539184a6cc6852c8d5869a4
Italo Paixão Gomes <italopaxg@gmail.com>
2025-04-28 00:06:33 -0300

    feat: create storage class

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,15 +1,28 @@
 export class Storage {
-  static getItem (key: string): string | null {
+  static getItemFromLocalStorage (key: string): string | null {
     return localStorage.getItem(key);
   }
 
-  static setItem (key: string, value: string): void {
+  static setItemInLocalStorage (key: string, value: string): void {
     localStorage.setItem(key, value);
   }
 
-  static setItems (objectKeys: { [key: string]: string }): void {
+  static setItemsInLocalStorage (objectKeys: { [key: string]: string }): void {
     Object.entries(objectKeys).forEach(([key, value]) => {
       localStorage.setItem(key, value);
     });
+  }
+
+  static getCookies (): { [key: string]: string } {
+    const stringCookiesArray = document.cookie.split(';');
+
+    const cookiesObject = stringCookiesArray.reduce((cookiesObject: { [key: string]: string }, cookieString: string) => {
+      const [cookieKey, cookieValue]: string[] = cookieString.trim().split('=');
+      cookiesObject[cookieKey] = cookieValue;
+  
+      return cookiesObject;
+    }, {});
+
+    return cookiesObject;
   }
 }
