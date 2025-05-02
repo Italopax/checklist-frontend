20377d64cb834de6ae693d2a75b995940efcedaa
Italo Paixão Gomes <italopaxg@gmail.com>
2025-05-01 22:34:55 -0300

    fix: change css var used

--- src/app/(public)/create-account/page.tsx
+++ src/app/(public)/create-account/page.tsx
@@ -2,7 +2,7 @@ import CreateUserForm from "@/components/createUserForm";
 
 export default function CreateUser () {
   return (
-    <section className="bg-(--borderDark) p-8 rounded-lg flex flex-col gap-8">
+    <section className="bg-(--border) p-8 rounded-lg flex flex-col gap-8">
       <div className="flex flex-col items-center gap-2">
         <h1>Criação de conta</h1>
         <p>Crie a sua conta no <strong>Checklist</strong>.</p>
--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/loginForm";
 
 export default function Login() {
   return (
-    <section className="bg-(--borderDark) p-8 rounded-lg flex flex-col gap-8">
+    <section className="bg-(--border) p-8 rounded-lg flex flex-col gap-8">
       <div className="flex flex-col items-center gap-2">
         <h1>Login</h1>
         <p>Realize o login na plataforma <strong>Checklist</strong>.</p>
