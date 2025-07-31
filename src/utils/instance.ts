4155ae498bdef66ff63bba4640cdde17cbe905ed
Italo Paixão Gomes <italopaxg@gmail.com>
2025-07-30 23:07:05 -0300

    feat: add axio interceptor to unauthorized errors

--- src/utils/instance.ts
+++ src/utils/instance.ts
@@ -1,9 +1,22 @@
+import { refershToken } from "@/actions/auth";
 import axios from "axios";
 
 export function getServerInstance () {
-  return axios.create({
+  const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     timeout: 10000,
     withCredentials: true,
   });
+
+  instance.interceptors.response.use((response) => response, async (error) => {
+    const stausCode = error.response.data.status;
+    if (stausCode === 401) {
+      await refershToken();
+      return instance(error.config);
+    }
+
+    return Promise.reject(error);
+  });
+
+  return instance;
 }
