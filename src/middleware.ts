cd05a881085ae167636de2f867cc9b34b8da3dfa
Italo Paixão Gomes <italopaxg@gmail.com>
2025-08-15 00:04:37 -0300

    feat: add logs on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,11 @@ export async function middleware (request: NextRequest) {
 
   const accessToken = request.cookies.get('accessToken');
   const refreshToken = request.cookies.get('refreshToken');
-  console.log('accessToken:', accessToken);
-  console.log('refreshToken:', refreshToken);
+  console.log(`accessToken: ${accessToken}`);
+  console.log(`refreshToken: ${refreshToken}`);
   
   const hasAuthorizationToken: boolean = Boolean(accessToken?.value || refreshToken?.value);
-  console.log('hasAuthorizationToken:', hasAuthorizationToken);
+  console.log(`hasAuthorizationToken: ${hasAuthorizationToken}`);
 
   console.log('unproctedRoutes.includes(request.nextUrl.pathname):', unproctedRoutes.includes(request.nextUrl.pathname))
 
@@ -24,6 +24,7 @@ export async function middleware (request: NextRequest) {
   } else {
     switch (true) {
       case !hasAuthorizationToken:
+        console.log('entrou aqui porra');
         return NextResponse.redirect(new URL(PagesRoutes.LOGIN, request.nextUrl));
       case hasAuthorizationToken:
         return NextResponse.next();
